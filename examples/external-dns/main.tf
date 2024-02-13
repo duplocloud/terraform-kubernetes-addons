604f6f1932306a60410edee61dd6fcc1db471ee2
Matt Herrick <matthew@duplocloud.net>
2024-02-13 15:48:46 -0600

    adding versions.tf

--- examples/external-dns/main.tf
+++ examples/external-dns/main.tf
@@ -35,7 +35,7 @@ terraform {
     }
     helm = {
       source  = "hashicorp/helm"
-      version = "2.11.0"
+      version = ">= 2.11.0"
     }
   }
 }
---
+++ modules/external-dns/versions.tf
@@ -0,0 +1,17 @@
+terraform {
+  required_version = ">= 1.4.4"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.12.0"
+    }
+    duplocloud = {
+      source  = "duplocloud/duplocloud"
+      version = ">= 0.10.0"
+    }
+    helm = {
+      source  = "hashicorp/helm"
+      version = ">= 2.11.0"
+    }
+  }
+}
