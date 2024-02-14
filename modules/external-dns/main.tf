32fba408acde07f35310f0a102f8f1759c2bab61
Matt Herrick <matthew@duplocloud.net>
2024-02-14 09:21:27 -0600

    incorrect role name

--- modules/external-dns/main.tf
+++ modules/external-dns/main.tf
@@ -2,7 +2,7 @@ locals {
   namespace = "duploservices-${var.tenant_name}"
   values = yamldecode(templatefile("${path.module}/values.yaml", {
     namespace              = local.namespace,
-    service_account_name   = local.namespace,
+    service_account_name   = "${local.namespace}-edit-user",
   }))
 }
 
