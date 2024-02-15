30f79c63f69030b1214c49c82b4643cca629aed1
Matt Herrick <matthew@duplocloud.net>
2024-02-15 15:59:44 -0600

    adding iam resources

---
+++ modules/external-dns/iam.tf
@@ -0,0 +1,29 @@
+# Extension for AWS DuploCloud tenant role for R53 permissions
+data "aws_iam_policy_document" "service_extension" {
+  count = local.iam_extension
+  statement {
+    sid = "externaldns"
+    actions = [
+      "route53:ChangeResourceRecordSets"
+    ]
+    resources = [
+      var.route53_zone_arn
+    ]
+  }
+  statement {
+    sid = "externaldnsglobal"
+    actions = [
+      "route53:ListHostedZones",
+      "route53:ListResourceRecordSets",
+      "route53:ListTagsForResource"
+    ]
+    resources = ["*"]
+  }
+}
+
+module "tenant-role" {
+  count           = local.iam_extension
+  source          = "github.com/duplocloud/terraform-duplocloud-components//modules/tenant-role-extension"
+  tenant_name     = var.tenant_name
+  iam_policy_json = data.aws_iam_policy_document.service_extension[0].json
+}
